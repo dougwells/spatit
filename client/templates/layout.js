24aac981b88903bfe97bb9c010e4e7b0c26503b0
dougwells <dougwells@gmail.com>
2015-11-02 14:08:39 -0700

    Finished up reviews.

--- client/templates/layout.js
+++ client/templates/layout.js
@@ -3,3 +3,29 @@ Template.registerHelper('truncateText', function(text, length){
     newText = newText.substr(0, Math.min(newText.length, newText.lastIndexOf(" ")));
     return new Spacebars.SafeString(newText);
 });
+
+//get rating average
+Template.registerHelper('getAvg', function(reviews){
+
+    if(reviews) {
+        var sum = 0;
+        for (var i = 0; i < reviews.length; i++) {
+            sum = sum + Number(reviews[i].rating);
+
+        }
+        var avg = sum / (reviews.length);
+        console.log(Math.round(avg).toString());
+        return Math.round(avg).toString();
+
+    }else{
+        return "notReviewed";
+    }
+});
+
+Template.registerHelper('getReviewsTotal', function(reviews){
+    if (reviews>0){
+        return reviews;
+    }else{
+        return 0;
+    }
+});
