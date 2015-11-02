deabcd08fdbe147954c4105f8094efc5b4cf3190
dougwells <dougwells@gmail.com>
2015-11-01 19:43:38 -0700

    Added reviews as an array of objects on the Products Collection

---
+++ client/templates/new_review/new_review.js
@@ -0,0 +1,25 @@
+Template.new_review.events({
+    "submit .new-review": function (event) {
+        var rating = event.target.rating.value;
+        var body = event.target.body.value;
+
+        Products.update({_id:this._id}, {
+            $push: {
+                reviews: {
+                    rating: rating,
+                    body: body
+                }
+            }
+        });
+
+        event.target.rating.value = "";
+        event.target.body.value = "";
+        FlashMessages.sendSuccess("Review Added");
+        Router.go('/');
+
+        return false;
+
+    }
+
+});
+
